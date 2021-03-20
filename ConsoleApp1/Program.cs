7237236da238f4e1be31239be95a7ba7ee84d1ab
Vladimir <shmalko89@list.ru>
2021-03-20 18:06:56 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadAllSettings();
+            AddUpdateAppSettings("Приветствую Вас!", "Сегодня "+ DateTime.Now.ToString("D"));
+            ReadAllSettings();
+        }
+
+        static void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("{0} {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                Console.WriteLine(result);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+    }
+}
---
+++ HomeTask8/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+
+namespace HomeTask8
+{
+    class Program
+    {
+        static void ReadSettings() // метод взят из Майкрософт докс
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("{0} {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+        static void AddUpdateAppSettings(string key, string value) // метод взят из Майкрософт докс
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            AddUpdateAppSettings("Приветствую Вас!", "Сегодня " + DateTime.Now.ToString("D"));
+            ReadSettings();
+            Console.WriteLine("Пожалуйста представьтесь, чтобы при следующем запуске программа учитывала Ваши данные!");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Отлично, а теперь скажите, сколько Вам лет?");
+            string UserAge = Console.ReadLine();
+            Console.WriteLine("Также укажите Вашу специальность!");
+            string UserSpecialty = Console.ReadLine();
+            AddUpdateAppSettings("Вас зовут " + UserName, "Вам " + UserAge + " лет");
+            AddUpdateAppSettings("Ваша специальность " + UserSpecialty, "");
+            Console.WriteLine("А теперь перезапустите приложение для отображения введенной Вами информации из настроек приложения");
+       }
+    }
+}
